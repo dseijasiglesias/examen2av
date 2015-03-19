d3d9b054c28930f59a97b4d7c303de3c7d36ac82
dseijasiglesias <dseijasiglesias@eniac08.danielcastelao.org>
2015-03-19 09:00:35 +0100

    añadida configuracion ConfigurationBuilder()

--- src/statustwitter/StatusTwitter.java
+++ src/statustwitter/StatusTwitter.java
@@ -6,6 +6,7 @@ import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 /**
  * Clase para recobrar los twitts del morrazo
@@ -18,13 +19,22 @@ public class StatusTwitter {
      * @throws twitter4j.TwitterException
      */
     public static void main(String[] args) throws TwitterException {
-     
-        Twitter mitwitter = new TwitterFactory().getInstance();
- 
+        
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("ggMDRpQngjGJgUGNjnsR39PpPRyl")
+                .setOAuthConsumerSecret("a1Uj5bKnisbjvnJN3OYUBH78BAYO9BN8A8Z4yMSj9MKsElkaN")
+                .setOAuthAccessToken("154877482-XrH4VUE0GejsibosAF3Ra8942bWmdI0adyVD1hR5v")
+                .setOAuthAccessTokenSecret("uL67zpwpMQ1MalbunabupbpOBGASObaovYUbaoygBBtaPbFhDMqQuSh");
+        
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();
+        
         Query query = new Query("#Cangas");
-        QueryResult result = mitwitter.search(query);
+        QueryResult result = twitter.search(query);
         for (Status status : result.getTweets()) {
-            System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+            System.out.println("@" + status.getUser().getScreenName() + ": " + status.getText());
         }
     }
 }
